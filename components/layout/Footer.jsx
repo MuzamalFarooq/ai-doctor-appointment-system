ae7a97fc7dc3c99b8db30f5f880e67486a1dc537
Muzamal Farooq <muzjani327@gmail.com>
2026-08-29 13:08:48 +0500

    aa

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -102,7 +102,7 @@ export function Footer() {
                 </div>
                 <div className="flex items-center gap-3.5 text-gray-400 hover:text-gray-300 transition-colors">
                   <Mail className="w-4 h-4 text-primary-400 flex-shrink-0" />
-                  <span>muzamalfarooq786@gmail.com</span>
+                  <span>muzamalfarooq111@gmail.com</span>
                 </div>
               </div>
             </div>
